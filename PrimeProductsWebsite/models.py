6e7588de4f9cf8061fa5d570e00725f4d2281515
BadRex <badr.md87@gmail.com>
2017-11-23 12:01:08 +0100

    Fix some inconsistencies in the DB models

--- PrimeProductsWebsite/models.py
+++ PrimeProductsWebsite/models.py
@@ -1,6 +1,52 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+    description = models.CharField(max_length=1000)
+
+
+class ContactInfo(models.Model):
+    location = models.CharField(max_length=50, db_index=True)
+    country = models.CharField(max_length=50, db_index=True)
+    phone_num = models.CharField(max_length=50, db_index=True)
+    email = models.EmailField(max_length=70,blank=True)
+
+class History(models.Model):
+    query = models.CharField(max_length=500)
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+    username = models.CharField(max_length=30, db_index=True, unique=True)
+    password = models.CharField(max_length=30)
+    contact = models.ForeignKey(ContactInfo)
+    #favorite = models.ForeignKey(Favorite)
+    histroy = models.ForeignKey(History)
+    join_date = models.DateTimeField(auto_now_add=True)
+
+class Review(models.Model):
+    stars = models.DecimalField(null=False, max_digits=1, decimal_places=0)
+    review_text = models.CharField(max_length=5000)
+    review_date = models.DateTimeField(auto_now_add=True)
+
+    # what exactly is the status here?
+    status = models.BooleanField(default=True)
+    author = models.ForeignKey(Client)
+    # whats is reports number?
+    #num_reports = models.IntegerField(null=False, min_value=0)
+
+class Supermarket(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+    contact = models.ForeignKey(ContactInfo)
+    opening_hours = models.CharField(max_length=50, db_index=True)
+    review = models.ForeignKey(Review)
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+    contact = models.ForeignKey(ContactInfo)
+    review = models.ForeignKey(Review)
+
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products')
     name = models.CharField(max_length=200, db_index=True)
@@ -22,64 +68,14 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     removed_at = models.DateTimeField(auto_now=True)
 
-    # Q: Can many-to-one relation be encoded with a ForeignKey
+    # Q: Can many-to-one relation be encoded with a ForeignKey?
     manufacturer = models.ForeignKey(Manufacturer)
     supermarket = models.ForeignKey(Supermarket)
     review = models.ForeignKey(Review)
-    offer = models.ForeignKey(Offer)
-    favorite = models.ForeignKey(Favorite)
+    #offer = models.ForeignKey(Offer)
+    #favorite = models.ForeignKey(Favorite)
     histroy = models.ForeignKey(History)
-
-
-class Client(models.Model):
-    name = models.CharField(max_length=50, db_index=True)
-    username = models.CharField(max_length=30, db_index=True, unique=True)
-    password = models.CharField(max_length=30)
-    contact = models.ForeignKey(ContactInfo)
-    review = models.ForeignKey(Review)
-    favorite = models.ForeignKey(Favorite)
-    histroy = models.ForeignKey(History)
-    join_date = models.DateTimeField(auto_now_add=True)
-
-
-class Review(models.Model):
-    stars = models.IntegerField(null=False, min_value=1, max_value=10)
-    review_text = models.CharField(max_length=5000)
-    review_date = models.DateTimeField(auto_now_add=True)
-
-    # what exactly is the status here?
-    status = models.BooleanField(default=True)
-
-    # whats is reports number?
-    num_reports = models.IntegerField(null=False, min_value=0)
-
-
-class Supermarket(models.Model):
-    name = models.CharField(max_length=50, db_index=True)
-    contact = models.ForeignKey(ContactInfo)
-    opening_hours = models.CharField(max_length=50, db_index=True)
-    review = models.ForeignKey(Review)
-
-
-class Manufacturer(models.Model):
-    name = models.CharField(max_length=50, db_index=True)
-    contact = models.ForeignKey(ContactInfo)
-    review = models.ForeignKey(Review)
-
-
-class ContactInfo(models.Model):
-    location = models.CharField(max_length=50, db_index=True)
-    country = models.CharField(max_length=50, db_index=True)
-    phone_num = models.CharField(max_length=50, db_index=True)
-    email = models.EmailField(max_length=70,blank=True)
-
-class Category(models.Model):
-    name = models.CharField(max_length=50, db_index=True)
-    description = models.CharField(max_length=1000)
 
 class Favorite(models.Model):
-    client = models.ForeignKey(Client)
+    #client = models.ForeignKey(Client)
     product =  models.ForeignKey(Product)
-
-class History(models.Model):
-    query = models.CharField(max_length=500)
